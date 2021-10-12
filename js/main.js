317ae7ad9fff5c2cb26e93f71231797cbbf1c75a
Lexleon-Oliver <lexleonmg@gmail.com>
2021-10-12 14:39:50 -0300

    Introduçao concluido

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var nome = "Lex Óliver";
+var idade = 31;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+// alert("Bem vindo "+nome+", Parabéns pelos seus "+idade+" anos.");
+alert(frase.replace("Japão","Brasil"));
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Japão","Brasil"));
